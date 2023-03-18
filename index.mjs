50e5138aa8245667b01a7afd1d0a29c2bc8b1d68
Toxi Woxi <75816946+ToxiWoxi@users.noreply.github.com>
2023-03-18 15:48:06 -0400

    Added detection for new hidden posts

--- index.mjs
+++ index.mjs
@@ -20,25 +20,52 @@ function publicEmbed (id) {
     return new EmbedBuilder()
     .setColor(0x188fef)
     .setURL(`https://blog.counter-strike.net/index.php/0000/00/${id}/`)
-    .setTitle('New public post detected!')
+    .setTitle('New public blog post detected!')
     .setDescription(`Id: \`${id}\``)
     .setThumbnail('https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/730/69f7ebe2735c366c65c0b33dae00e12dc40edbe4.jpg')
 }
-function hiddenEmbed (id) {
+function hiddenPostEmbed (id) {
     return new EmbedBuilder()
     .setColor(0xffff00)
     .setThumbnail('https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/730/69f7ebe2735c366c65c0b33dae00e12dc40edbe4.jpg')
-    .setTitle('New blog activity detected!')
+    .setTitle('New hidden blog post detected!')
+    .setDescription(`Id: \`${id}\``)
+}
+function hiddenActivityEmbed (id) {
+    return new EmbedBuilder()
+    .setColor(0xffff00)
+    .setThumbnail('https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/730/69f7ebe2735c366c65c0b33dae00e12dc40edbe4.jpg')
+    .setTitle('New hidden blog activity detected!')
     .setDescription(id.length >= 2 ? `Ids: \`${id[0]}\` - \`${id[id.length - 1]}\`` : `Id: \`${id}\``)
 }
 
-async function fetchCheck(id) {
+async function fetchCheckGeneral(id) {
     return new Promise (resolve => {
         fetch(`https://blog.counter-strike.net/wp-json/wp/v2/categories?post=${id}`)
             .then(res => res.json()) 
             .then(json => {
                 if (json.data?.status) {
                     if (json.data.status === 400) {
+                        resolve(0)
+                    } else {
+                        resolve(json.data.status)
+                    }
+                } else resolve(300)
+            })
+            .catch(err => {
+                console.log(err)
+                resolve(0)
+            })
+    })
+}
+
+async function fetchCheckNew(id) {
+    return new Promise (resolve => {
+        fetch(`https://blog.counter-strike.net/wp-json/wp/v2/posts/${id}`)
+            .then(res => res.json()) 
+            .then(json => {
+                if (json.data?.status) {
+                    if (json.data.status === 404) {
                         resolve(0)
                     } else {
                         resolve(json.data.status)
@@ -59,13 +86,13 @@ client.once(Events.ClientReady, async (bot) => {
     let user = await bot.users.fetch(userENV)
     let channel = await bot.channels.fetch(channelENV)
 
-    let hiddenIds = []
+    let hiddenActivityIds = []
     let recentDetection = false
-    async function sendHidden () {
+    async function sendHiddenActivity () {
         new Promise(resolve => {
-            if (hiddenIds.length) {
-                let currentEmbed = hiddenEmbed(hiddenIds)
-                hiddenIds = []
+            if (hiddenActivityIds.length) {
+                let currentEmbed = hiddenActivityEmbed(hiddenActivityIds)
+                hiddenActivityIds = []
                 user.send({embeds: [currentEmbed]})
                 channel.send({embeds: [currentEmbed]}).then(message => {
                     message.crosspost()
@@ -88,45 +115,77 @@ client.once(Events.ClientReady, async (bot) => {
         })
     }
 
+    async function sendHiddenPost(id) {
+        new Promise(resolve => {
+            if (id) {
+                let currentEmbed = hiddenPostEmbed(current)
+                user.send({embeds: [currentEmbed]})
+                channel.send({embeds: [currentEmbed]}).then(message => {
+                    message.crosspost()
+                        .then(resolve())
+                })
+            } else {resolve()}
+        })
+    }
+
     // Fetch loop
     setInterval(async () => {
-        await fetchCheck(current).then(async (res) => {
+        await fetchCheckGeneral(current).then(async (res) => {
             // Nothing with ID; check one further in case a number is skipped.
             if (!res) {
-                await fetchCheck(current + 1).then((res) => {
+                await fetchCheckGeneral(current + 1).then((res) => {
                     if (res) {
+                        sendHiddenActivity()
                         incNum()
-                        sendHidden()
                     }
                 })
             }
             // Hidden activity
             if (res === 401) {
-                console.log(`hidden - ${current}`)
-                hiddenIds.push(current)
-                recentDetection = true
-                incNum()
+                await fetchCheckNew(current).then(async (res) => {
+                    if (!res) {
+                        console.log(`hidden - ${current}`)
+                        hiddenActivityIds.push(current)
+                        recentDetection = true
+                        incNum()
+                    }
+                    if (res === 401) {
+                        console.log(`hidden (new) - ${current}`)
+                        sendHiddenActivity().then(() => {
+                            sendHiddenPost(current)
+                        })
+                        incNum()
+                    }
+                    // This shouldn't be possible, but I'm just covering my grounds
+                    if (res === 300) {
+                        console.log(`public - ${current}`)
+                        sendHiddenActivity().then(() => {
+                            sendPublic(current)
+                        })
+                        incNum()
+                    }
+                })
             }
             // Public blog post
             if (res === 300) {
                 console.log(`public - ${current}`)
-                sendHidden().then(() => {
+                sendHiddenActivity().then(() => {
                     sendPublic(current)
                 })
                 incNum()
             }
         })
 
-    }, 1000)
+    }, 2000)
     
     // Send loop
     setInterval(async () => {
         if (recentDetection) {
             recentDetection = false
         } else {
-            sendHidden()
+            sendHiddenActivity()
         }
-    }, 5000)
+    }, 10000)
 });
 
 client.login(tokenENV);
